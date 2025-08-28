f4cb380a4bbd17162c05ffcf2500fbb606f2a4c2
ani <129322594+anaalvesg@users.noreply.github.com>
2025-08-28 18:55:51 -0300

    WIP: iniciando validacoes do registro

---
+++ front/src/pages/register/register.js
@@ -0,0 +1,25 @@
+document.querySelector("#confirm-button").addEventListener("click", function () { 
+    let msg = ''
+
+    if (document.querySelector("#name").value.length < 3 || !isNaN(document.querySelector("#name").value))  {
+        msg+= 'Nome inválido'
+    }
+
+    if (document.querySelector("#cpf").value.length != 11 && !isNaN(document.querySelector("#cpf").value)) {
+        msg+= '\n CPF inválido'
+    }
+
+    // if (document.querySelector("#birth-date").value) {
+
+    // }
+
+    if (document.querySelector("#password").value.length < 6) {
+        msg+= '\n Senha fraca'
+    }
+
+    if (document.querySelector("#confirm-password").value != document.querySelector("#password").value) {
+        msg+= '\n Senhas não coincidem'
+    }
+
+    console.log(msg)
+})
